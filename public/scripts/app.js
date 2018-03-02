7b6164a0e360139112c12d34a8680cfc872f605f
jahnava <jahnavalmt@gmail.com>
2018-03-02 11:09:55 -0500

    Started writing code using JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,8 +15,43 @@ var template = React.createElement(
     'p',
     null,
     'this is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
   )
 );
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Jahnava'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age:28'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Florida'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -5,8 +5,22 @@ var template = (
 <div>
   <h1>Indecision App</h1>
   <p>this is some info</p>
+  <ol>
+    <li>Item one</li>
+    <li>Item two</li>
+  </ol>
 </div>
 );
+
+var templateTwo = (
+  <div>
+    <h1>Jahnava</h1>
+    <p>Age:28</p>
+    <p>Location: Florida</p>
+  </div>
+);
+
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
